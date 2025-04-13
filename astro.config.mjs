d519036de6ca3de188a25d28fe3c0b2595fcbc4a
ToddLadner <tladner03@gmail.com>
2025-04-13 00:13:29 -0500

    Initial push

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import alpinejs from '@astrojs/alpinejs';
+import icon from 'astro-icon';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [mdx(), sitemap(), alpinejs(), icon()],
+  experimental: {
+    preserveScriptOrder: true,
+    responsiveImages: true,
+    svg: true,
+  },
+});
