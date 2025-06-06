cbdc2ff38974fee2c422511e6885dfcb0372d7ec
ToddLadner <tladner03@gmail.com>
2025-06-06 06:32:02 -0500

    refactor blog to use content collections

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    category: z.string(),
+    tags: z.array(z.string()).optional(),
+    layout: z.string().optional()
+  })
+});
+
+export const collections = { blog };
